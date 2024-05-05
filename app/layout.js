185c9d5969a77d02d7badaea1c3c31acf99a4006
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-05-05 23:12:19 +0600

    chore: Add lazy loading for images and update layout

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+import { getDocuments } from "@/lib/doc";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,14 +11,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // const allDocuments = getDocuments();
-  // console.log(allDocuments);
+  const allDocuments = getDocuments();
+  console.log(allDocuments);
 
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="h-full lg:ml-72 xl:ml-80">
-          {/* <Header docs={allDocuments} /> */}
+          <Header docs={allDocuments} />
           <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
             <main className="flex-auto py-16">
               <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
