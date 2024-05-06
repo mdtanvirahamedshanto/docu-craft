a9370697abd0007e38746992d90f4e4abdb99363
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-05-06 14:24:56 +0600

    Update layout and remove console.log statements

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   const allDocuments = getDocuments();
-  console.log(allDocuments);
 
   return (
     <html lang="en">
